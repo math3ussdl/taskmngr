298d2ed760bf05ab65e4644f0e83182a6a7fdab0
Hugo Lima <limabrot879@gmail.com>
2024-02-09 17:19:38 -0300

    chore: add bootstrap

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {}
